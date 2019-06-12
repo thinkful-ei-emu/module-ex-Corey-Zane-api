8d2130d2ad6a7f0515d5ac90b8ab3dd203a486f0
Corey Moore <monorthwest@gmail.com>
2019-06-12 08:16:24 -0700

    tested AJAX call to api

---
+++ scripts/api.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const api = (function(){
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/coreyZane';
+  function getItems () {
+    return Promise.resolve('A successful response!');
+  }
+  return {
+    getItems,
+  };
+
+})();
